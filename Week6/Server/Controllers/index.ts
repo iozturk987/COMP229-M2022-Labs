3117440c176a20d90eed82aa442a0cfd890cd7b5
Ibrahim <iozturk987@gmail.com>
2022-06-22 17:14:03 -0400

    configured the user to be detected

--- Week6/Server/Controllers/index.ts
+++ Week6/Server/Controllers/index.ts
@@ -1,26 +1,28 @@
 import express from 'express';
 
+import { UserDisplayName } from '../Util';
+
 export function DisplayHomePage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Home', page: 'home' });
+    res.render('index', { title: 'Home', page: 'home', displayName: UserDisplayName(req) });
 }
 
 export function DisplayAboutPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'About Us', page: 'about' });
+    res.render('index', { title: 'About Us', page: 'about', displayName: UserDisplayName(req)  });
 }
 
 export function DisplayProjectsPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Projects', page: 'projects' });
+    res.render('index', { title: 'Our Projects', page: 'projects', displayName: UserDisplayName(req)  });
 }
  
 export function DisplayServicesPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Services', page: 'services' });
+    res.render('index', { title: 'Our Services', page: 'services', displayName: UserDisplayName(req)  });
 }
 
 export function DisplayContactPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Contact Us', page: 'contact' });
+    res.render('index', { title: 'Contact Us', page: 'contact', displayName: UserDisplayName(req)  });
 }
